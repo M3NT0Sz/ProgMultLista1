612327a3ca7f1239a529535337e67449caef43f7
M3NT0Sz <matheusmendes2005@outlook.com>
2025-02-14 15:05:24 -0300

    Exercicio 14 - Feito

--- exer14.js
+++ exer14.js
@@ -1,1 +1,6 @@
 const prompt = require('prompt-sync')();
+const capital = prompt('Digite o capital: ');
+const taxa = prompt('Digite a taxa de juros: ');
+const tempo = prompt('Digite o tempo: ');
+const montante = parseFloat(capital) * (1 + parseFloat(taxa) / 100) ** parseFloat(tempo);
+console.log(`O montante é: R$${montante.toFixed(2)}`);

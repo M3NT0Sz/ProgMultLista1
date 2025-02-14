8171add5c68e4d54af449e82ff3dc370f78a86bb
M3NT0Sz <matheusmendes2005@outlook.com>
2025-02-14 15:01:40 -0300

    Exercicio 11 - Feito

--- exer11.js
+++ exer11.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-const peso = prompt('Digite o peso: ');
+const peso = prompt('Digite o peso em KG: ');
 const altura = prompt('Digite a altura: ');
 const imc = parseFloat(peso) / (parseFloat(altura) ** 2);
 console.log(`O IMC é: ${imc.toFixed(2)}`);
